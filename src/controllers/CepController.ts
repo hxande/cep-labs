457c380b9866f5180bc21b52955444aa15666bdf
Hugo <c1297097@bb.com.br>
2020-08-29 19:03:42 -0300

    Dados mocados, pois api externa saiu do ar

--- src/controllers/CepController.ts
+++ src/controllers/CepController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { CepModel } from '../models/CepModel';
-import axios from 'axios';
+// import axios from 'axios';
+import data from '../../dataMock.json';
 
 class CepController {
 
@@ -8,29 +9,33 @@ class CepController {
 
         let { id } = request.params;
         let localidade: CepModel = new CepModel('', '', '', '', '', '', '', '', '', '');
-
+        console.log(data);
         const validarFormato = new RegExp(/^[0-9]{8}$/);
 
         if (validarFormato.test(id)) {
-            try {
-                let i = 1;
-                do {
-                    const { status, statusText, data } = await axios.get(`https://viacep.com.br/ws/${id}/json/`);
-                    console.log(status);
-                    console.log(statusText);
-                    console.log(data);
-                    data.hasOwnProperty('cep') ? localidade = data : id = id;
-                    console.log(this.substituirZeros(id, i));
-                    i++;
-                } while (localidade.cep === '' || id === '00000000');
+            // Estava utilizando uma API grátis para consulta de CEP,
+            // porém durante o desenvolvimento a mesma caiu e não consegui
+            // mais continuar o desenvolvimento da solução, optando por 'mocar'.
 
-                return response.json({ status: 200, data: localidade });
+            // try {
+            //     let i = 1;
+            //     do {
+            //         const { status, statusText, data } = await axios.get(`https://viacep.com.br/ws/${id}/json/`);
+            //         console.log(status);
+            //         console.log(statusText);
+            //         console.log(data);
+            //         data.hasOwnProperty('cep') ? localidade = data : id = id;
+            //         console.log(this.substituirZeros(id, i));
+            //         i++;
+            //     } while (localidade.cep === '' || id !== '00000000');
 
-            } catch (error) {
-                console.error(error)
-            }
+            //     return response.json({ status: 200, data: localidade });
+
+            // } catch (error) {
+            //     return response.json({ status: 400, message: error.message });
+            // }
         } else {
-            return response.json({ status: 400, message: 'Favor informar os dados no formato correto.' });
+            return response.json({ status: 400, message: 'CEP inválido.' });
         }
     }
 
